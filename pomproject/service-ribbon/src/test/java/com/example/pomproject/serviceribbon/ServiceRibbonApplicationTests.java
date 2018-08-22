66ab26b62c03bef5e53101d0bb4cecb641154902
cfeicose2 <42602062+cfeicose2@users.noreply.github.com>
2018-08-22 15:49:32 +0800

    Add files via upload

---
+++ pomproject/service-ribbon/src/test/java/com/example/pomproject/serviceribbon/ServiceRibbonApplicationTests.java
@@ -0,0 +1,24 @@
+package com.example.pomproject.serviceribbon;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.stereotype.Service;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ServiceRibbonApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
+
+
